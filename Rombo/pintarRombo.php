3c0595aa49d232cc671133c0f2acca7b4ff737c4
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2015-10-19 22:04:30 +0200

    rombo hueco
    para practicar

--- Rombo/pintarRombo.php
+++ Rombo/pintarRombo.php
@@ -12,49 +12,62 @@ $espacios1 = $altura - 4;
 $espacios2 = 1;
 $caracter = 3;
 
+//Pintamos los espacios del principio
 for ($a = 0; $a<$espacios1; $a++){
     echo "<img src='imagenes/blanco.jpg'>";
 }
-
+//Como el vertice primero no cambia lo pintamos del tirón
 echo "<img src='imagenes/almohada.jpg'> <br>";
 $espacios1--;
 
+//Nuestro bucle principal que controla las veces que tiene 
+//que repetir el proceso
 for ($b = 0; $b<$altura1; $b++){
+    //Pintamos los espacios del principio
     for ($c = 0; $c<$espacios1; $c++){
         echo "<img src='imagenes/blanco.jpg'>";
     }
+    //Pintamos el caracter que forma el rombo
     for ($d = 0; $d<$caracter; $d++){
         echo "<img src='imagenes/almohada.jpg'>";
     }
     echo "<br>";
+//sumamos en dos el caracter
 $caracter+=2;
+//Disminuimos los espacios en blanco
 $espacios1--;
 }
 
-
+//Pintamos la linea del medio del rombo
 for ($e = 0; $e<$caracter; $e++){
     echo "<img src='imagenes/almohada.jpg'>";
 }
 echo "<br>";
+//Decrementamos en dos los caracteres 
 $caracter-=2;
+//Bucle principal 
 for ($a = 0; $a<$altura2; $a++){
+//Pintamos los espacios del principio
     for ($b = 0; $b<$espacios2; $b++){
         echo "<img src='imagenes/blanco.jpg'>";
     }
+    //Pintamos los caracteres
     for ($c = 0; $c<$caracter; $c++){
         echo "<img src='imagenes/almohada.jpg'>";
     }
     echo "<br>";
+    //Los caracteres van a menos en cada vuelta
 $caracter-=2;
+//Los espacios aumentan 
 $espacios2++;
 }
 
 
-
+//Pintamos los espacios en blanco 
 for ($d = 0; $d<$espacios2; $d++){
     echo "<img src='imagenes/blanco.jpg'>";
 }
-
+//Pintamos el último vertice del rombo
 echo "<img src='imagenes/almohada.jpg'> <br>";
 ?>
 
--- Rombo2/pintarRombo.php
+++ Rombo2/pintarRombo.php
@@ -6,6 +6,7 @@ $altura2 = ($altura - 3)/2;
 $espacios1 = $altura - 4;
 $espacios2 = 1;
 $caracter = 3;
+$espaciosMedio = 1;
 
 for ($a = 0; $a<$espacios1; $a++){
     echo "<img src='imagenes/blanco.jpg'>";
@@ -15,33 +16,43 @@ echo "<img src='imagenes/almohada.jpg'> <br>";
 $espacios1--;
 
 for ($b = 0; $b<$altura1; $b++){
-    for ($a = 0; $a<$espacios1; $a++){
+    for ($c = 0; $c<$espacios1; $c++){
         echo "<img src='imagenes/blanco.jpg'>";
     }
-    for ($c = 0; $c<$caracter; $c++){
-        echo "<img src='imagenes/almohada.jpg'>";
+    echo "<img src='imagenes/almohada.jpg'>";
+    for ($d = 0; $d<$espaciosMedio; $d++){
+       echo "<img src='imagenes/blanco.jpg'>";
     }
+    echo "<img src='imagenes/almohada.jpg'>";
     echo "<br>";
 $caracter+=2;
 $espacios1--;
+$espaciosMedio+=2;
 }
 
 
-for ($d = 0; $d<$caracter; $d++){
+echo "<img src='imagenes/almohada.jpg'>";
+    for ($e = 0; $e<$espaciosMedio; $e++){
+       echo "<img src='imagenes/blanco.jpg'>";
+    }
     echo "<img src='imagenes/almohada.jpg'>";
-}
-echo "<br>";
+    echo "<br>";
+    
 $caracter-=2;
+$espaciosMedio-=2;
 for ($a = 0; $a<$altura2; $a++){
     for ($b = 0; $b<$espacios2; $b++){
         echo "<img src='imagenes/blanco.jpg'>";
     }
-    for ($c = 0; $c<$caracter; $c++){
-        echo "<img src='imagenes/almohada.jpg'>";
+    echo "<img src='imagenes/almohada.jpg'>";
+    for ($c = 0; $c<$espaciosMedio; $c++){
+       echo "<img src='imagenes/blanco.jpg'>";
     }
+    echo "<img src='imagenes/almohada.jpg'>";
     echo "<br>";
 $caracter-=2;
 $espacios2++;
+$espaciosMedio-=2;
 }
 
 
