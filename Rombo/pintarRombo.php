4074b2fe4d3c08e024c4a52f9fb440285ec25598
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2015-10-19 21:30:23 +0200

    rombo
    para practicar

--- Rombo/pintarRombo.php
+++ Rombo/pintarRombo.php
@@ -1,6 +1,11 @@
 <?php
 $altura = $_POST['altura'];
 
+// Tenemos en cuenta que para pintar el rombo tenemos que quitar
+// los vertices del principio y la linea del medio por lo tanto 
+// si la altura introducida es 7 restamos los vertices y la linea del medio
+//lo divido para ver los saltos que tiene que dar el bucle sin tener en cuenta 
+// el vertice y la linea del medio
 $altura1 = ($altura - 3)/2;
 $altura2 = ($altura - 3)/2;
 $espacios1 = $altura - 4;
@@ -15,10 +20,10 @@ echo "<img src='imagenes/almohada.jpg'> <br>";
 $espacios1--;
 
 for ($b = 0; $b<$altura1; $b++){
-    for ($a = 0; $a<$espacios1; $a++){
+    for ($c = 0; $c<$espacios1; $c++){
         echo "<img src='imagenes/blanco.jpg'>";
     }
-    for ($c = 0; $c<$caracter; $c++){
+    for ($d = 0; $d<$caracter; $d++){
         echo "<img src='imagenes/almohada.jpg'>";
     }
     echo "<br>";
@@ -27,7 +32,7 @@ $espacios1--;
 }
 
 
-for ($d = 0; $d<$caracter; $d++){
+for ($e = 0; $e<$caracter; $e++){
     echo "<img src='imagenes/almohada.jpg'>";
 }
 echo "<br>";
