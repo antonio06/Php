8df67e6372667ab591d8315a1a1c830dc79f3816
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2015-10-19 21:01:45 +0200

    php
    ejercicio de rombo sólido para practicar

---
+++ Rombo/Rombo.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Introduce 10 números
+        <form action="pintarRombo.php" method="post">
+            <input type="number" name="altura" min="0" required="">
+            <input type="submit" value="Introducir" >
+        </form>
+    </body>
+</html>
