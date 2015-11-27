11fa977ef32dc39d302710aab1feaf2bb0460e53
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2015-11-27 13:37:40 +0100

    php
    para Github

--- Practica 7/Practica3/borrado.php
+++ Practica 7/Practica3/borrado.php
@@ -1,11 +1,4 @@
 <?php
 
-/* $_SESSION['conectado'] = NULL;
-  $_SESSION["usuario"] = NULL;
-  $_SESSION['carrito'] = "";
-  $_SESSION["codigo"] = NULL; */
-unset($_SESSION["usuario"]);
-unset($_SESSION["carrito"]);
-unset($_SESSION["codigo"]);
-unset($_SESSION['conectado']);
+session_destroy();
 header("Location: ../Practica3.php");
--- Practica 7/Practica4/borrado.php
+++ Practica 7/Practica4/borrado.php
@@ -1,7 +1,3 @@
 <?php
-
-
-unset($_SESSION["usuario"]);
-unset($_SESSION['conectado']);
-unset($_SESSION['carrito']);
+session_destroy();
 header("Location: ../Practica4.php");
