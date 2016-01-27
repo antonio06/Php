3891f3cbe57f8da653d6ba81b40a07c987ea71af
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-01-27 13:23:57 +0100

    php
    para github

--- TrabajaoPHPyAJAX/Modelo/ActividadesDB.php
+++ TrabajaoPHPyAJAX/Modelo/ActividadesDB.php
@@ -12,6 +12,15 @@ class ActividadesDB {
     private static $user = 'root';
     private static $password = 'root';
 
+    /**
+     * Este método se encargará de conectarnos a la base de datos actividades.
+     * @param string $server nombre del servidor.
+     * @param string $db nombre de la base de datos.
+     * @param string $user nombre de usuario en la base de datos (root).
+     * @param string $password contraseña del usuario en la base de datos (root).
+     * @return string  si todo a sido correcto se conecta a la base de datos,
+     * en caso contrario nos devuelve un mensaje de error.
+     */
     public static function connectDB() {
         try {
             $connection = new PDO("mysql:host=" . self::$server . ";dbname=" . self::$db . ";charset=utf8", self::$user, self::$password);
