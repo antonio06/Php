d500d70a105d8cb33934ae92d9f90c97a7a156aa
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-01-27 09:33:02 +0100

    php
    para github

---
+++ TrabajaoPHPyAJAX/Modelo/ActividadesDB.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of ActividadesDB
+ *
+ * @author Antonio Contreras Román
+ */
+class ActividadesDB {
+
+    private static $server = 'localhost';
+    private static $db = 'actividades';
+    private static $user = 'root';
+    private static $password = 'root';
+
+    public static function connectDB() {
+        try {
+            $connection = new PDO("mysql:host=" . self::$server . ";dbname=" . self::$db . ";charset=utf8", self::$user, self::$password);
+        } catch (PDOException $e) {
+            echo "No se ha podido establecer conexión con el servidor de bases de datos.<br>";
+            die("Error: " . $e->getMessage());
+        }
+
+        return $connection;
+    }
+
+}
