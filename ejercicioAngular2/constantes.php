beae556bbbc6475c1d0413cb88c9c3ee3b47d4f1
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-01-28 10:28:41 +0100

    php con angular
    para github

---
+++ ejercicioAngular2/constantes.php
@@ -0,0 +1,8 @@
+<?php
+
+// Constantes para la conexion BD
+        const HOST = "localhost";
+        const NOMBRE_DB = "europa";
+        const USUARIO_DB = "root";
+        const CLAVE_DB = "";
+?>
