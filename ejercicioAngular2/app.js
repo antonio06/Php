cca9ed6883e189f98071a2bf8f3c7b2511650974
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-02-04 09:12:08 +0100

    php con angular
    para github

---
+++ ejercicioAngular2/app.js
@@ -0,0 +1,15 @@
+(function(){
+	var app= angular.module("musica",[]);
+		
+	app.controller('artistasController',function ($scope,$http) {
+		    $http.get("musicajson.php").success(function(data) {
+				$scope.artistas = data;
+			});
+			
+	});	
+	
+})();
+
+
+
+
