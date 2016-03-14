932747b3794254f24b05749112bfbf8861397f03
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-03-14 17:00:08 +0100

    proyecto
    en proceso

--- ProyectoBin/Controller/partePrivada/gestionPersonas.php
+++ ProyectoBin/Controller/partePrivada/gestionPersonas.php
@@ -2,15 +2,13 @@
 
 session_start();
 
-// Cargamos la librería de twig y las clases de la base de datos de BindDB y 
-// de la clase de actividad
 require_once '../twig/lib/Twig/Autoloader.php';
 require_once '../../Model/BinDb.php';
 require_once '../../Model/Persona.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../../View/partePrivada');
 $twig = new Twig_Environment($loader);
-//$personas = Persona::getPersonas();
+
 $limite = 2;
 $numeroPaginas = Persona::getNumeroPaginas($limite);
 
@@ -28,7 +26,7 @@ if (!isset($_GET['pagina'])) {
     $personas = Persona::getPersonasByLimit($_SESSION['pagina'], $limite);
     echo $twig->render('gestionPersonas.html.twig', ["personas" => $personas, "arrayNumeros" => $arrayNumeros]);
 } else {
-    $_SESSION['pagina'] = Persona::getSesionPagina($pagina, $limite, $_SESSION['pagina']);
+    $_SESSION['pagina'] = Persona::getSesionPagina($_GET['pagina'], $limite, $_SESSION['pagina']);
     $personas = Persona::getPersonasByLimit($_SESSION['pagina'], $limite);
     echo $twig->render('gestionPersonas.html.twig', ["personas" => $personas, "arrayNumeros" => $arrayNumeros]);
 }
