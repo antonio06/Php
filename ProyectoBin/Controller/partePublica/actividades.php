fdd35d27d10a949d69051936a343e02c8da900d2
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-03-21 23:52:58 +0100

    proyecto
    en proceso

--- ProyectoBin/Controller/partePublica/actividades.php
+++ ProyectoBin/Controller/partePublica/actividades.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 // Cargamos la librería de twig y las clases de la base de datos de BindDB y 
 // de la clase de actividad
@@ -9,6 +10,7 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../../View/partePublica');
 $twig = new Twig_Environment($loader);
 $descriptores = Actividad::getDescriptoresActividad();
+
 $estados = Actividad::getEstadosActividad();
 $limite = 2;
 $numeroPaginas = Actividad::getNumeroPaginas($limite);
@@ -19,17 +21,29 @@ for ($i = 1; $i <= $numeroPaginas; $i++) {
         $arrayNumeros[$auxi++] = $i;
     }
 }
-
-if (!isset($_GET['pagina'])) {
-    $pagina = 1;
-    $_SESSION['pagina'] = 1;
-    $_SESSION['pagina'] = Actividad::getSesionPagina($pagina, $limite, $_SESSION['pagina']);
-    $actividades = Actividad::getActividadesByLimit($_SESSION['pagina'], $limite);
-    echo $twig->render('actividades.html.twig', ["actividades" => $actividades, "arrayNumeros" => $arrayNumeros]);
-} else {
-    $_SESSION['pagina'] = Actividad::getSesionPagina($_GET['pagina'], $limite, $_SESSION['pagina']);
-    $actividades = Actividad::getActividadesByLimit($_SESSION['pagina'], $limite);
-    echo $twig->render('actividades.html.twig', ["actividades" => $actividades, "arrayNumeros" => $arrayNumeros]);
+if (isset($_SESSION['logeado'])) {
+    if (!isset($_GET['pagina'])) {
+        $pagina = 1;
+        $_SESSION['pagina'] = 1;
+        $_SESSION['pagina'] = Actividad::getSesionPagina($pagina, $limite, $_SESSION['pagina']);
+        $actividades = Actividad::getActividadesByLimit($_SESSION['pagina'], $limite);
+        echo $twig->render('actividades.html.twig', ["actividades" => $actividades, "arrayNumeros" => $arrayNumeros, "email" => $_SESSION['email']]);
+    } else {
+        $_SESSION['pagina'] = Actividad::getSesionPagina($_GET['pagina'], $limite, $_SESSION['pagina']);
+        $actividades = Actividad::getActividadesByLimit($_SESSION['pagina'], $limite);
+        echo $twig->render('actividades.html.twig', ["actividades" => $actividades, "arrayNumeros" => $arrayNumeros, "email" => $_SESSION['email']]);
+    }
+}else{
+    if (!isset($_GET['pagina'])) {
+        $pagina = 1;
+        $_SESSION['pagina'] = 1;
+        $_SESSION['pagina'] = Actividad::getSesionPagina($pagina, $limite, $_SESSION['pagina']);
+        $actividades = Actividad::getActividadesByLimit($_SESSION['pagina'], $limite);
+        echo $twig->render('actividades.html.twig', ["actividades" => $actividades, "arrayNumeros" => $arrayNumeros]);
+    } else {
+        $_SESSION['pagina'] = Actividad::getSesionPagina($_GET['pagina'], $limite, $_SESSION['pagina']);
+        $actividades = Actividad::getActividadesByLimit($_SESSION['pagina'], $limite);
+        echo $twig->render('actividades.html.twig', ["actividades" => $actividades, "arrayNumeros" => $arrayNumeros]);
+    }
 }
-
 
--- ProyectoBin/Controller/partePublica/detalles.php
+++ ProyectoBin/Controller/partePublica/detalles.php
@@ -1,10 +1,17 @@
 <?php
+
 require_once '../twig/lib/Twig/Autoloader.php';
 require_once '../../Model/BinDb.php';
 require_once '../../Model/Actividad.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../../View/partePublica');
 $twig = new Twig_Environment($loader);
-$actividad = Actividad::getActividadByCodigo($_GET['codigo']);
+if ($_SESSION['logeado'] == "Si") {
+    $actividad = Actividad::getActividadByCodigo($_GET['codigo']);
 
-echo $twig->render('detalle_formulario.html.twig', ["actividad" => $actividad]);
+    echo $twig->render('detalle_formulario.html.twig', ["actividad" => $actividad, "email" => $_SESSION['email']]);
+}else{
+    $actividad = Actividad::getActividadByCodigo($_GET['codigo']);
+
+    echo $twig->render('detalle_formulario.html.twig', ["actividad" => $actividad]);
+}
