d936bcb3bb93c2fab46ea9709246edde6e13935f
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-03-28 20:56:22 +0200

    proyecto
    en proceso

--- ProyectoBin/Controller/partePrivada/panelPrincipal.php
+++ ProyectoBin/Controller/partePrivada/panelPrincipal.php
@@ -7,6 +7,6 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../../View/partePrivada');
 $twig = new Twig_Environment($loader);
 if ($_SESSION['logeado'] == "Si"){
+    
    echo $twig->render('panelPrincipal.html.twig', ["email" => $_SESSION['email']]);
-
 }
