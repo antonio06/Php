bf732c40ce44b80cca5cd1d11da3671f83758bcf
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-04-11 22:42:55 +0200

    proyecto
    en proceso

--- ProyectoBin/actividadesbin.sql
+++ ProyectoBin/actividadesbin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-03-2016 a las 21:55:52
+-- Tiempo de generación: 11-04-2016 a las 22:41:54
 -- Versión del servidor: 10.1.9-MariaDB
 -- Versión de PHP: 5.6.15
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `actividad` (
   `codigo_actividad` int(50) NOT NULL,
   `titulo` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `estado` set('En proyecto','Abierto plazo de solicitud','Finalizado plazo de solicitud','Actividades en desarrollo','Terminadas') COLLATE utf8_spanish2_ci NOT NULL,
+  `estado` set('En proyecto','Abierto plazo de solicitud','Finalizado plazo de solicitud','Actividades en desarrollo','Terminadas') COLLATE utf8_spanish2_ci NOT NULL DEFAULT 'En proyecto',
   `coordinador` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `ponente` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `ubicacion` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
@@ -61,7 +61,9 @@ INSERT INTO `actividad` (`codigo_actividad`, `titulo`, `estado`, `coordinador`, 
 (12, 'Curso Móviles ', 'Abierto plazo de solicitud', 'Eva H', 'Manuel Orta', 'Antonio', '2016-03-11', '2016-03-12', '13:00:00', '15:00:00', 2, 5, 'No', 'Primaria', 'Curso de móviles para niños '),
 (13, 'Internet para padres ', 'Actividades en desarrollo', 'Conchi Román', 'María Victoria Contreras', 'C/Random 9', '2016-04-15', '2016-04-16', '09:30:00', '11:30:00', 2, 3, 'No', 'Padres', 'Curso de internet para padres nivel principiante '),
 (14, 'Costura para pequeños', 'Abierto plazo de solicitud', 'Leticia', 'Pablo', 'C/Random 4', '2016-03-11', '2016-03-13', '12:00:00', '14:00:00', 2, 2, 'Si', 'Infantil', 'Costura para pequeños'),
-(15, 'Practicas Para Embarazos', 'En proyecto', 'Susana', 'Luis', 'C/Salitre', '2016-03-31', '2016-04-02', '13:00:00', '14:00:00', 1, 5, 'No', 'Padres', 'Practicas para madres embarazadas');
+(15, 'Practicas Para Embarazos', 'En proyecto', 'Susana', 'Luis', 'C/Salitre', '2016-03-31', '2016-04-02', '13:00:00', '14:00:00', 1, 5, 'No', 'Padres', 'Practicas para madres embarazadas'),
+(21, 'adsad', 'Finalizado plazo de solicitud', '', '', '', '0000-00-00', '0000-00-00', '00:00:00', '00:00:00', 0, 0, 'Si', 'Profesores de primaria', ''),
+(22, 'sasdfs', 'En proyecto', '', '', '', '0000-00-00', '0000-00-00', '00:00:00', '00:00:00', 0, 0, 'No', 'Padres', '');
 
 -- --------------------------------------------------------
 
@@ -80,7 +82,15 @@ CREATE TABLE `participa` (
 --
 
 INSERT INTO `participa` (`codigo_persona`, `codigo_actividad`, `codigo_perfil`) VALUES
-(21, 1, 1);
+(21, 1, 1),
+(24, 9, 3),
+(24, 13, 3),
+(29, 1, 4),
+(29, 4, 4),
+(36, 1, 4),
+(36, 4, 4),
+(36, 7, 4),
+(36, 12, 4);
 
 -- --------------------------------------------------------
 
@@ -144,12 +154,12 @@ INSERT INTO `persona` (`codigo`, `DNI`, `nombre`, `apellido1`, `apellido2`, `per
 (26, '9999-W', 'Alejandro', 'Sanchez', 'Nuñez', 3, '', 'hombre', '2016-03-16', 'C/Random 4', 'Málaga', 'Málaga', 'España', '2016-03-14', '2016-03-16', '9999-W', '147235-V', 'alejandro@gmail.com', '$2y$10$ktwrYDGtmAlg0kgjN5wDqeO7EPCs1sBVtODAhz2t1f/K8sKGTWdQG', 'Usuario', 'Usuario Alejandro'),
 (27, '22222-J', 'Luis', 'Sanchez', 'Castejón', 2, 'Hydrangeas.jpg', 'hombre', '2016-03-21', 'C/Salmonete 4', 'Málaga', 'Málaga', 'España', '2016-03-14', '2016-03-15', '22222.J', '147529-O', 'luis@gmail.com', '$2y$10$dptckPed2UX/jUZoFa4iMefd418yIXs78iB4Sjb14EE2W0QlIqgnG', 'Usuario', 'Usuario Luis'),
 (28, '88888-A', 'Juan', 'Fernandez', 'Ruiz', 2, '1.jpg', 'hombre', '2016-03-14', 'C/Aprobado', 'Málaga', 'Málaga', 'España', '2016-03-14', '2016-03-15', '888888-A', '427639-P', 'juan@gmail.com', '$2y$10$hnK9tunhGtH4u2sHkc2woO3epw7/O7JqQZX4tLYdDOKB3zdsaEqEu', 'Limitado', 'Una persona llamada Juan'),
-(29, '2222-P', 'Luisa', 'Gutierrez', 'Franco', 5, '2.jpg', 'mujer', '2016-03-07', 'C/Salmonete 3', 'Málaga', 'Málaga', 'España', '2016-03-08', '2016-03-14', '2222-P', '235871-L', 'luisa@gmail.com', '$2y$10$rSbMWhTQcYnoUE7HPF6cIepmMLdFWrcrWtrh7QiMOJ/mn/4aUoJVO', 'Usuario', 'Una persona llamada Luisa'),
+(29, '2222-PÑ', 'Luisa', 'Gutierrez', 'Franco', 5, 'Jellyfish.jpg', 'mujer', '2016-03-07', 'C/Salmonete 3', 'Málaga', 'Málaga', 'España', '2016-03-08', '2016-03-14', '2222-P', '235871-LO', 'luisa@gmail.com', '$2y$10$BunTXNpnKJNuuYhHd59OIOY6gOw7hCgVHohqwNY8Oc1kpt2yFWwPq', 'Usuario', 'Una persona llamada Luisa'),
 (30, '5555-Ñ', 'Carmela', 'Contreras', 'Frias', 3, 'Tulips.jpg', 'mujer', '2016-03-01', 'C/Aprobado 9', 'Málaga', 'Málaga', 'España', '2016-03-14', '2016-03-20', '8888-Ñ', '748639-P', 'carmela@gmail.com', '$2y$10$sJKbmZEclPBHQ/G.Go6na.v7pLBjRlwGf3nHr.SqaY.h0nny3/MyC', 'Usuario', 'Una persona llamada Carmela'),
 (31, '9999-L', 'María', 'Nuñez', 'Lopez', 4, 'Jellyfish.jpg', 'mujer', '2016-03-14', 'C/Aprobado 4', 'Málaga', 'Málaga', 'España', '2016-03-14', '2016-03-15', '9999-L', '15839-O', 'maria@gmail.com', '$2y$10$reUM9RUe2esJ6JubzYJgvOzbCUDvw3xspBrkpSwRBsB9Y6gjwWxGS', 'Limitado', 'Una persona llamada María'),
 (32, '77777-I', 'Iñaki', 'Gurruchaga', 'Ñoñez', 1, 'Lighthouse.jpg', 'mujer', '2016-02-15', 'C/Random 11', 'Málaga', 'Málaga', 'España', '2016-03-15', '2016-03-18', '77777-I', '48239-k', 'inaki@gmail.com', '$2y$10$3Bxdq29SFnYrzOeopal0/uhRTw7IEw/0246paREEsJ0Dc8AuZ6/gq', 'Usuario', 'Una persona llamada Iñaki'),
 (34, '44444-T', 'Marta', 'Contreras', 'Ruíz', 2, 'Tulips.jpg', 'mujer', '2016-01-18', 'C/Aprobado 14', 'Málaga', 'Málaga', 'España', '2016-03-08', '2016-03-31', '44444-T', '992571-T', 'marta@gmail.com', '$2y$10$bVAYmU/JsIzaPjVgUCcJC.n2Rpge.B4TND9cTsTc3TMn9ivYSaGrG', 'Usuario', 'Una persona llamada Marta'),
-(36, '44444-TP', 'Antonio', 'Contreras', 'Román', 1, 'Chrysanthemum.jpg', 'hombre', '2016-03-14', 'C/Random 1', 'Málaga', 'Málaga', 'España', '2016-03-31', '2016-04-03', '888888-Ao', '38475-It', 'antonio@gmail.com', '$2y$10$RvNId5cjEv1tIwB6fBwTVOju4VuTr.LFDDm84gDW53z9MwtySXhGK', 'Administrador', 'Una persona llamada Antonio'),
+(36, '44444-TPX', 'Antonio', 'Contreras', 'Román', 1, 'Tulips.jpg', 'hombre', '2016-03-14', 'C/Random 1', 'Málaga', 'Málaga', 'España', '2016-03-31', '2016-04-03', '888888-Ao', '38475-Itx', 'antonio@gmail.com', '$2y$10$3IWXW0gZdy1GmX.Vs6WUu.iWp1.mt3uQJ/Abq5Rk2aQd0sDT6xumi', 'Administrador', 'Una persona llamada Antonio'),
 (37, '33333-LO', 'Pablo', 'Iglesias', 'Turrion', 1, 'Tulips.jpg', 'hombre', '2016-03-31', 'C/Aprobado 18', 'Málaga', 'Málaga', 'España', '2016-04-01', '2016-04-03', '37418-I', '64172-ÑU', 'pablo@gmail.com', '$2y$10$mV/cddRMHSOeH2fasV.tC.ycbCsBy3LPQCXhkiG9FPzH5Gw9lKHha', 'Administrador', 'Una persona llamada Pablo');
 
 --
@@ -191,7 +201,7 @@ ALTER TABLE `persona`
 -- AUTO_INCREMENT de la tabla `actividad`
 --
 ALTER TABLE `actividad`
-  MODIFY `codigo_actividad` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `codigo_actividad` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT de la tabla `perfil`
 --
@@ -201,7 +211,7 @@ ALTER TABLE `perfil`
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `codigo` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `codigo` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 --
 -- Restricciones para tablas volcadas
 --
