463d4047e5ff5a2ccf3678274f130e7a3a5d607d
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-05-04 09:46:37 +0200

    proyecto
    en proceso

---
+++ ProyectoBin/public/asset/js/misActividades.js
@@ -0,0 +1,40 @@
+/* 
+ * Funciones con peticiones AJAX y jquery.
+ */
+
+// Función principal
+$(function () {
+    $(document).on("click", "#paginacion > li", function (event) {
+        event.preventDefault();
+        $("#divMensaje").removeClass("correcto error").addClass("oculto").html("");
+
+        // Si estoy en la misma página que el número del botón que no haga nada.
+        var paginaBoton = parseInt($(event.currentTarget).attr("data-pagina"));
+        if (parseInt($("#paginacion").attr("data-pagina")) !== paginaBoton) {
+            paginar(paginaBoton);
+        }
+
+    });
+});
+
+
+function paginar(pagina) {
+    $.ajax({
+        url: '/Controller/partePrivada/usuario/misActividades.php',
+        method: 'GET',
+        data: {
+            pagina: pagina
+        }, success: function (tabla, textStatus, jqXHR) {
+            // Petición con éxito
+            if (textStatus === "success") {
+                $("#contenedorTabla").html(tabla);
+            }
+
+        }, error: function (jqXHR, textStatus, errorThrown) {
+            // La petición por algún motivo ha fallado
+            $("#divMensaje").show().html("Ha habido un error al solicitar los datos, inténtalo más tarde.");
+        }
+    });
+}
+
+
