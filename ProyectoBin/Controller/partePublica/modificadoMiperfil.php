a571316efeb4831242a4c6fa1442a31e8666f80e
AntonioContreraRomán <antoniocontrerasroman@gmail.com>
2016-05-17 23:23:35 +0200

    proyecto
    en proceso

--- ProyectoBin/Controller/partePublica/modificadoMiperfil.php
+++ ProyectoBin/Controller/partePublica/modificadoMiperfil.php
@@ -11,13 +11,29 @@ $twig = new Twig_Environment($loader);
 if ($_SESSION['logeado'] == "Si") {
     switch ($_POST['opcion']) {
         case "modificar":
-            move_uploaded_file($_FILES['foto']['tmp_name'], "../../public/asset/img/" . $_FILES['foto']['name']);
-            //$perfil = Persona::getCodigoPerfilbyDescripcion($_POST['perfil']);
-            $email = "";
-            $password = Persona::getPasswordByCodigo($_SESSION['codigo']);
-            $persona = new Persona($_SESSION['codigo'], $_POST['DNI'], $_POST['nombre'], $_POST['apellido1'], $_POST['apellido2'], $_POST['perfil'], $_FILES['foto']['name'], $_POST['sexo'], $_POST['fecha_nac'], $_POST['direccion'], $_POST['municipio'], $_POST['provincia'], $_POST['pais'], $_POST['fecha_alta'], $_POST['fecha_baja'], $_POST['n_Seguridad_Social'], $_POST['n_Cuenta_Bancaria'], $_POST['email'], $password, $_POST['perfil_usuario'], $_POST['observaciones']);
+            $foto = file_get_contents($_FILES['foto']['tmp_name']);
+            /*
+            * Para la foto en vez de utilizar el método de almacenarlo en un fichero,
+            * modificamos el campo foto en la BBDD de string a longblod, luego en el php
+            * sobre el $_FILES que nos devuelve el html le hacemos un file_get_contents,
+            * que transformara el fichero a cadena a eso le hacemos un addslashes,
+            * que añadirá barras sobre los caracteres y a eso un base64_encode para
+            * codificarlo a base 64 
+            * MIRAR DOCUMENTACIÓN OFICIAL DE PHP
+            * addslashes:http://php.net/manual/es/function.addslashes.php
+            * file_get_contents: http://php.net/manual/es/function.file-get-contents.php
+            *  base64_encode: http://php.net/manual/es/function.base64-encode.php
+            */
+        $foto = addslashes(base64_encode($foto));
+            $persona = new Persona($_SESSION['codigo'], $_POST['DNI'],
+                    $_POST['nombre'], $_POST['apellido1'], $_POST['apellido2'],
+                    $_POST['perfil'], $foto, $_POST['sexo'],
+                    NULL, $_POST['direccion'], $_POST['municipio'],
+                    $_POST['provincia'], $_POST['pais'], NULL,
+                    NULL, $_POST['n_Seguridad_Social'],
+                    $_POST['n_Cuenta_Bancaria'], $_POST['email'], NULL,
+                    $_POST['perfil_usuario'], $_POST['observaciones']);
             $persona->update();
-            print_r($persona);
             header('Location: actividades.php');
             break;
         case "cancelar":
